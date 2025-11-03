d3c02dbd9f63f45d8725e9ca94097a4d93f43fd7
Van Tam Pham <pvantam2003@gmail.com>
2025-11-03 13:47:08 +0200

    Hello World

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div >
+      <div style={{justifyContent: 'center', alignItems: 'center', display: 'flex', height: '100vh', flexDirection: 'column'}}>
+        <h1>
+          Hello World 
+        </h1>
+        <p>
+          Theater Web Application
+        </p>
+        <p>
+          Deployed on Azure
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
