3eb485675fb77f739bb1b365a29eca6c61919177
Zhi-Lin-Tan <ztan25@students.oamk.fi>
2025-11-28 06:41:35 +0000

    new update, merge the code from staff

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import StaffNavbar from "./components/navbars/StaffNavBar";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Location from './pages/customer/Location';
+import StaffHomePage from './pages/staff/StaffHomePage';
+import StaffSetTimes from './pages/staff/StaffSetTimes';
+import StaffSetSeat from './pages/staff/StaffSetSeat';
+import StaffSeeTable from './pages/staff/StaffSeeTable';
 
 function App() {
   return (
     <BrowserRouter>
-      <StaffNavbar />
+
       <Routes>
-        <Route path="/location" element={<Location />} />
+        <Route path="/*" element={<Location />} />
+
+        <Route path="/StaffHomePage/:id" element={<StaffHomePage />} />
+        <Route path="/StaffSetTimes/:id/:auditorium" element={<StaffSetTimes />} />
+        <Route path="/StaffSetSeat/:id/:auditorium" element={<StaffSetSeat />} />
+        <Route path="/StaffSeeTable/:id/:auditorium" element={<StaffSeeTable />} />
       </Routes>
     </BrowserRouter>
   );
