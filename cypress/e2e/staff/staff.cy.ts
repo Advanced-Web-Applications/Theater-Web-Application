25f44bb9cdc5cebbd96c5e11d8651d8f65267b40
Zhi-Lin-Tan <ztan25@students.oamk.fi>
2025-12-11 18:12:42 +0000

    update login/logout cypress test

--- cypress/e2e/staff/staff.cy.ts
+++ cypress/e2e/staff/staff.cy.ts
@@ -1,46 +1,56 @@
 describe('Staff Authentication', () => {
   
   beforeEach(() => {
+    cy.intercept('GET', '**/api/staff/theaters/*', {
+      statusCode: 200,
+      body: {
+        id: 1,
+        name: 'Oulu Theater',
+        rooms: [] 
+      }
+    }).as('getTheater');
     cy.intercept('POST', '**/api/auth/login', {
       statusCode: 200,
       body: {
         token: 'fake-jwt-token-123456',
-        role: 'staff',
-        theater_id: 1,
-        city: 'Oulu',
-        user: { 
-          id: 1, 
+        role: 'staff',      
+        theater_id: 1,      
+        theaterId: 1,       
+        city: 'Oulu',       
+        user: {
+          id: 1,
           email: 'staff@test.com',
           name: 'Test Staff'
         }
       }
     }).as('loginRequest');
 
-    //
     cy.visit('/login', {
       onBeforeLoad: (win) => {
         win.localStorage.setItem('selectedCity', 'Oulu') 
       }
     })
 
-    // type in email and password, and submit
     cy.get('input[type="email"]').type('any_staff@test.com') 
     cy.get('input[type="password"]').type('any_password')
     cy.get('button[type="submit"]').click()
     
-    //waiting for login request to complete
     cy.wait('@loginRequest');
 
     cy.url().should('include', '/StaffHomePage')
   })
 
   it('should redirect to dashboard after login', () => {
-      //try to verify navbar exists
+      cy.wait('@getTheater');
+      
       cy.get('.staffNavTheaterName').should('exist')
   })
 
   it('should logout successfully', () => {
+    cy.wait('@getTheater');
+
     cy.get('.staffNavLogout').click()
+    
     cy.url().should('include', '/login')
   })
 })
